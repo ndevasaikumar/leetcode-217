2b50f3031c7e74ca44e898b68dac47f1b5706aab
ndevasaikumar <119063711+ndevasaikumar@users.noreply.github.com>
2024-10-06 12:49:54 +0530

    Update l217.cpp

--- l217.cpp
+++ l217.cpp
@@ -1,3 +1,5 @@
+/* Method 1 */
+
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
@@ -7,3 +9,23 @@ public:
                 }
             return false;}
     };
+
+
+/*Method 2 */
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        map<int,int> mp;
+        for(int i = 0; i < nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        map<int,int> ::iterator it = mp.begin();
+        while(it != mp.end()){
+            if(it -> second > 1)
+                return true;
+            it++;
+        }
+        return false;
+    }
+};
